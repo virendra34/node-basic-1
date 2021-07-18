0b550aef97cc20c82f5e997908ab134a112b03bd
virendra34 <yvirendra34@gmail.com>
2021-07-19 01:45:17 +0530

    complted task 1

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,14 @@
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
-
+router.get('/', (req, res) => {
+    let { page, limit } = req.query;
+    limit = limit || 3;
+    page = page || 1;
+    // console.log(page, limit, (page - 1) * 3);
+    // console.log(pagenatedRes)
+    return res.status(200).send(recipes.slice(((page - 1) * limit), limit * page));
+});
 
 
 
